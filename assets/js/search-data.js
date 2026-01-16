03ff76f63eb317df31dc04e38577103045cd4cda
shwetabhatt08 <shwetabhatt08@gmail.com>
2026-01-16 12:30:01 +0000

    Deploying to gh-pages from @ shwetabhatt08/shwetabhatt08.github.io@2033fa5a62b46d58bb4d018c61947ebe60577049 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -508,7 +508,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%73%68%77%65%74%61%62%68%61%74%74%30%38@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -529,7 +529,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=HuwzwUwAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
