75b47c0978f9142a502c2d81f12a5a419c59d2ff
shwetabhatt08 <shwetabhatt08@gmail.com>
2026-01-16 11:47:31 +0000

    Deploying to gh-pages from @ shwetabhatt08/shwetabhatt08.github.io@8be4b5800b37e0646538703efe335b46e56a15be 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "shwetabhatt08/shwetabhatt.github.io",
+    "data-repo": "shwetabhatt08/shwetabhatt08.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
