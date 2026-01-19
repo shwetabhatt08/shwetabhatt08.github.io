b3be401629317351a8aeb4df4a7a8322a2bdfaac
shwetabhatt08 <shwetabhatt08@gmail.com>
2026-01-19 10:44:51 +0000

    Deploying to gh-pages from @ shwetabhatt08/shwetabhatt08.github.io@6bf99c9e64d0d89f04ca4d8a2d9e63bb917f1936 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/blog/";
+            window.location.href = "https://medium.com/@shweta_bhatt/index.html";
           },
         },{id: "nav-publications",
           title: "publications",
