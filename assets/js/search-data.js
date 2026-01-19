e47266f15660d59efa16d6ee5c1ce5c24ec5f6bc
shwetabhatt08 <shwetabhatt08@gmail.com>
2026-01-19 08:57:43 +0000

    Deploying to gh-pages from @ shwetabhatt08/shwetabhatt08.github.io@8cc0787f9077187734eae7852c9f8f38e3cfc6c6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -448,8 +448,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-invited-as-a-chief-guest-and-judge-for-crack-a-thon-at-presidency-university-bangalore",
+          title: 'Invited as a Chief Guest and Judge for Crack-a-thon at Presidency University, Bangalore...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
